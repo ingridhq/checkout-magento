08c73503ceaa32f4c6872adb6d067fa1e44a5e9c
Sergiu Moscalu <sergiu@nordicwebteam.se>
2021-04-20 07:31:21 +0000

    NGRD-17 hide magento shipping methods

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/shipping': {
+                'Ingrid_Checkout/js/view/shipping-mixin': true
+            }
+        }
+    }
+};
