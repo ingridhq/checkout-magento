93ed86b5efd2fc27f8de82f2550f57e07380971a
Sergiu Moscalu <sergiu@nordicwebteam.se>
2022-09-09 09:04:52 +0000

    NGRD-35 fix Deprecations and add guzzlehttp

--- Api/Siw/Model/Address.php
+++ Api/Siw/Model/Address.php
@@ -409,7 +409,7 @@ class Address implements ModelInterface, ArrayAccess {
      * @return $this
      */
     public function setCountry($country) {
-        $this->container['country'] = strtoupper($country);
+        $this->container['country'] = mb_strtoupper($country, 'utf-8');
 
         return $this;
     }
@@ -464,7 +464,7 @@ class Address implements ModelInterface, ArrayAccess {
      *
      * @return boolean
      */
-    public function offsetExists($offset) {
+    public function offsetExists($offset): bool {
         return isset($this->container[$offset]);
     }
 
@@ -475,7 +475,7 @@ class Address implements ModelInterface, ArrayAccess {
      *
      * @return mixed
      */
-    public function offsetGet($offset) {
+    public function offsetGet($offset): mixed {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
 
@@ -487,7 +487,7 @@ class Address implements ModelInterface, ArrayAccess {
      *
      * @return void
      */
-    public function offsetSet($offset, $value) {
+    public function offsetSet($offset, $value): void {
         if (is_null($offset)) {
             $this->container[] = $value;
         } else {
@@ -502,7 +502,7 @@ class Address implements ModelInterface, ArrayAccess {
      *
      * @return void
      */
-    public function offsetUnset($offset) {
+    public function offsetUnset($offset): void {
         unset($this->container[$offset]);
     }
 
--- Api/Siw/Model/CreateSessionRequest.php
+++ Api/Siw/Model/CreateSessionRequest.php
@@ -492,7 +492,7 @@ class CreateSessionRequest implements ModelInterface, ArrayAccess {
      *
      * @return boolean
      */
-    public function offsetExists($offset) {
+    public function offsetExists($offset): bool {
         return isset($this->container[$offset]);
     }
 
@@ -503,7 +503,7 @@ class CreateSessionRequest implements ModelInterface, ArrayAccess {
      *
      * @return mixed
      */
-    public function offsetGet($offset) {
+    public function offsetGet($offset): mixed {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
 
@@ -515,7 +515,7 @@ class CreateSessionRequest implements ModelInterface, ArrayAccess {
      *
      * @return void
      */
-    public function offsetSet($offset, $value) {
+    public function offsetSet($offset, $value): void {
         if (is_null($offset)) {
             $this->container[] = $value;
         } else {
@@ -530,7 +530,7 @@ class CreateSessionRequest implements ModelInterface, ArrayAccess {
      *
      * @return void
      */
-    public function offsetUnset($offset) {
+    public function offsetUnset($offset): void {
         unset($this->container[$offset]);
     }
 
